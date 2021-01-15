33aeed0227e2cf95a073fc06c6ce8d4d8c3b6f81
lennsen <lennjen@gmail.com>
2021-01-15 17:51:15 +0100

    installed pundit and implemented search for location

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,11 @@
 class SpacesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
   def index
-    @spaces = Space.all
+    if params[:location].present?
+      @spaces = policy_scope(Space).near(params[:location], 5, units: :km)
+    else
+      @spaces = policy_scope(Space).all
+    end
   end
 end
---
+++ app/policies/space_policy.rb
@@ -0,0 +1,8 @@
+class SpacePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+end
