3fbedd299512c49e6c813545d98feada47cecdad
lennsen <lennjen@gmail.com>
2021-01-17 16:58:30 +0100

    edits css on index and adds radio buttons to spaces#new form

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -9,7 +9,7 @@ class SpacesController < ApplicationController
     end
   end
 
-  def show 
+  def show
     @space = Space.find(params[:id])
     # @booking = Booking.new
     authorize @space
@@ -31,13 +31,17 @@ class SpacesController < ApplicationController
     @space.user = current_user
     @space.save
     if @space.save
-      redirect_to van_path(@space)
+      redirect_to space_path(@space)
     else
       render :new
     end
     authorize @space
   end
 
-  
+  private
+
+  def set_params
+    params.require(:space).permit(:title, :description, :space_type, :photo, :price_per_month, :location)
+  end
 
 end
--- app/policies/space_policy.rb
+++ app/policies/space_policy.rb
@@ -12,4 +12,20 @@ class SpacePolicy < ApplicationPolicy
     true
   end
 
+  def create?
+    true
+  end
+
+  # def destroy?
+  #   record.user == user
+  # end
+  #
+  # def update?
+  #   record.user == user
+  # end
+  #
+  # def edit?
+  #   record.user == user
+  # end
+
 end
