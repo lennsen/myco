be2f9e5d5bf625d10933298adc157ac22dbcc010
Matthias LÃuecke <mattisluecke@gmail.com>
2021-01-19 18:25:44 +0100

    booking#create and space#view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 
-  resources :spaces
+  resources :spaces do
+    resources :bookings, only: %i[create]
+  end
+
+  # resources :bookings, only: [:index, :show, :edit, :update, :destroy]
+
+
 end
