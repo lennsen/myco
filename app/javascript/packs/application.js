9d0ab1c85c5eda35e480b3394337ff1c00bf39b3
lennsen <lennjen@gmail.com>
2021-01-28 14:50:18 +0100

    map js fix, still needs to be styled

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -9,7 +10,9 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import "mapbox-gl/dist/mapbox-gl.css";
 
+// internal imports
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initMapbox } from '../plugins/init_mapbox';
 
