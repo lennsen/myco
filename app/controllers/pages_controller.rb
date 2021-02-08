1bfdafcae26c731a71afd363b9f90a2d5c43cb72
lennsen <lennjen@gmail.com>
2021-02-08 12:01:51 +0100

    card styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @private_spaces = Space.where(space_type: "Private Space")
-    @community_spaces = Space.where(space_type: "Community Space")
-    @corporate_spaces = Space.where(space_type: "Corporate Space")
-    @professional_spaces = Space.where(space_type: "Professional Space")
+    @private_spaces = Space.where(space_type: "PRIVATE")
+    @community_spaces = Space.where(space_type: "COMMUNITY")
+    @corporate_spaces = Space.where(space_type: "CORPORATE")
+    @professional_spaces = Space.where(space_type: "PROFESSIONAL")
   end
 end
