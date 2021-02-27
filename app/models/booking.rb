aa82b3921e2ce4923fc37ff3992b1dde781a3c3d
lennsen <lennjen@gmail.com>
2021-02-27 15:48:53 +0100

    adds validations for Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :space
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :message_to_host, presence: true
 end
